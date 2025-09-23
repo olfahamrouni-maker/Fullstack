f2b39257879fffd66c640a0fc1982726eee4fe50
olfahamrouni-maker <backup.olfa.hamrouni@gmail.com>
2025-09-23 02:34:24 +0100

    Ajout du dossier domo comme dossier normal

--- src/main/java/com/domo/domo/model/Projet.java
+++ src/main/java/com/domo/domo/model/Projet.java
@@ -1,10 +1,6 @@
 package com.domo.domo.model;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
